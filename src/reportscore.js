6b5ecd51693b09491421c64d4b4adacdce4f7973
aly11 <aly11@duke.edu>
2023-07-18 21:41:25 -0400

    clear button

--- src/reportscore.js
+++ src/reportscore.js
@@ -1,20 +1,8 @@
 import {useState, useRef, useEffect } from 'react';
 import { useComponentVisible } from './DetectExternalClick.js';
+import { func } from 'prop-types';
 
 const dropdownOptions = new Set(["a","b","c","ab","bc","ac","abc"])
-
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn') && !event.target.matches('#myInput') ) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     for (var i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// };
-
 
 function DropdownSearch({sendSearchInput, inputRef}){
 
@@ -69,16 +57,12 @@ function Player({filter, name, setSelectionHandler}){
 }
 
 
-function Dropdown({availablePlayers, setAvailablePlayers}){
-    const {
-        ref,
-        isComponentVisible,
-        setIsComponentVisible
-    } = useComponentVisible(false);
-
-    const [selection, setSelection] = useState('');
+function Dropdown({availablePlayers, setAvailablePlayers, selection, setSelection}){
+    const {ref, isComponentVisible, setIsComponentVisible} = useComponentVisible(false);
     const [filter, setFilter] = useState('');
+    // const [selection, setSelection] = useState('');
     const inputRef = useRef(null);
+    
 
     useEffect(() => {
         if (isComponentVisible){
@@ -89,6 +73,7 @@ function Dropdown({availablePlayers, setAvailablePlayers}){
     const toggleDropdown = () => {
         if(!isComponentVisible){
             setIsComponentVisible(true)
+            setFilter('')
         }
     }
     
@@ -151,23 +136,49 @@ function Dropdown({availablePlayers, setAvailablePlayers}){
 
 export default function ReportScore(){
     const [availablePlayers, setAvailablePlayers] = useState(dropdownOptions);
-    
+    const [winner1, setWinner1] = useState('');
+    const [winner2, setWinner2] = useState('');
+    const [winner3, setWinner3] = useState('');
+    const [loser1, setLoser1] = useState('');
+    const [loser2, setLoser2] = useState('');
+    const [loser3, setLoser3] = useState('');
+
+    function clearSelection(){
+        const newItems = [winner1,winner2,winner3,loser1,loser2,loser3]
+        for (let val of newItems){
+            if (val !== ""){
+                setAvailablePlayers(prevSet => new Set(prevSet.add(val)));
+            }
+        }
+        setWinner1('')
+        setWinner2('')
+        setWinner3('')
+        setLoser1('')
+        setLoser2('')
+        setLoser3('')
+    }
+
     return(
         <div class="scoreReport">
             <div class="winningSide">
                 <h3>Winners</h3>
-                
                 <Dropdown
                     availablePlayers={availablePlayers}
                     setAvailablePlayers={setAvailablePlayers}
+                    selection={winner1}
+                    setSelection={setWinner1}
+                />                
+                <Dropdown 
+                    availablePlayers={availablePlayers}
+                    setAvailablePlayers={setAvailablePlayers}
+                    selection={winner2}
+                    setSelection={setWinner2}
                 />
                 <Dropdown 
                     availablePlayers={availablePlayers}
                     setAvailablePlayers={setAvailablePlayers}
-                />
-                <Dropdown 
-                    availablePlayers={availablePlayers}
-                    setAvailablePlayers={setAvailablePlayers}
+                    selection={winner3}
+                    setSelection={setWinner3}
                 />
             </div>
             <br></br>
@@ -176,18 +187,24 @@ export default function ReportScore(){
                 <Dropdown 
                     availablePlayers={availablePlayers}
                     setAvailablePlayers={setAvailablePlayers}
+                    selection={loser1}
+                    setSelection={setLoser1}
                 />
                 <Dropdown 
                     availablePlayers={availablePlayers}
                     setAvailablePlayers={setAvailablePlayers}
+                    selection={loser2}
+                    setSelection={setLoser2}
                 />
                 <Dropdown 
                     availablePlayers={availablePlayers}
                     setAvailablePlayers={setAvailablePlayers}
+                    selection={loser3}
+                    setSelection={setLoser3}
                 />
             </div>
-            <button>Submit</button>
-            <button>Clear</button>  {/* TODO */}
+            <button>Submit</button>  {/* TODO */}
+            <button onClick={clearSelection}>Clear</button>  {/* TODO */}
 
         </div>
     )
