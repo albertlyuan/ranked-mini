6a317977eb3875cf5b4d996b46f697040ba2f1bf
aly11 <aly11@duke.edu>
2023-07-23 18:04:30 -0400

    remvoed brimstone

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-      <h1 class="clickable" onClick={() => setTab('leaderboard')}>Brimstone Ranked Mini</h1>
+      <h1 class="clickable" onClick={() => setTab('leaderboard')}>Ranked Mini</h1>
       <div className="toolbar">
         <TabButton
           isActive={tab === 'leaderboard'}
